881ad4889433ae62c5da2d6d2636c7b20f1a4a93
Jon Carlson <jon@jomca.co>
2014-07-25 09:55:17 -0600

    process.env.PORT fix

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', function(req, res) {
 	res.send('<h1>Hello Boulder</h1>');
 });
 
-var server = app.listen(7057, function() {
+var server = app.listen(process.env.PORT || 9323, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
